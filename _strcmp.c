a73e6e4f04e79de131be306b33c66f5f9b6cf1b5
RMoraa <moraajillo@gmail.com>
2023-09-20 04:39:29 -0700

    the function that compares two strings

---
+++ _strcmp.c
@@ -0,0 +1,27 @@
+#include "shell.h"
+
+/**
+ *_strcmp - compare strings
+ *@s1: the first string
+ *@s2: the second string
+ *
+ * Return: int
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int diff = 0, index1;
+
+	index1 = 0;
+	while (s1[index1] != '\0' && s2[index1] != '\0')
+	{
+		diff = s1[index1] - s2[index1];
+
+		if (diff == 0)
+			index1++;
+		else
+			break;
+	}
+
+	return (diff);
+}
